4135bfdee5966b1c59e3c270f244b02b165183ea
piyushimraw <piyush.srivastavaimraw@gmail.com>
2019-04-14 16:39:49 +0530

    Showing approvers count on request

--- pages/campign/requests.js
+++ pages/campign/requests.js
@@ -9,6 +9,7 @@ class Requests extends Component {
   state = {
     data: [],
     tableData: [],
+    requestCount: '',
     manager: ''
   };
   async componentDidMount() {
@@ -18,34 +19,26 @@ class Requests extends Component {
     const manager = await CampaingInstance.methods.manager().call();
     const request = await CampaingInstance.methods.requestCount().call();
     const requestCount = web3.utils.hexToNumber(request._hex);
-    let data = Array.from({length: requestCount}, (_, i) => CampaingInstance.methods.getRequest(i+1).call());
-        data = await Promise.all(data);
+    let data = Array.from({ length: requestCount }, (_, i) =>
+      CampaingInstance.methods.getRequest(i + 1).call()
+    );
+    data = await Promise.all(data);
     const tableData = data.map((d, i) => ({
-      id: i+1,
+      id: i + 1,
       desc: d._desc,
       beneficary: d._beneficary,
       value: web3.utils.hexToNumber(d._val._hex),
       isCompleted: d.isCompleted,
       appoversCount: web3.utils.hexToNumber(d._contribCount._hex)
     }));
-    // for (let i = 1; i <= requestCount; i++) {
-    //   const request = await CampaingInstance.methods.getRequest(i).call();
-    //   data.push({
-    //     id: i,
-    //     desc: request._desc,
-    //     beneficary: request._beneficary,
-    //     value: web3.utils.hexToNumber(request._val._hex),
-    //     isCompleted: request.isCompleted,
-    //     appoversCount: web3.utils.hexToNumber(request._contribCount._hex)
-    //   });
-    // }
     this.setState({
       data: tableData,
+      requestCount,
       manager
     });
   }
   render() {
-    const { data } = this.state;
+    const { data, requestCount } = this.state;
     return (
       <Layout>
         <Table
@@ -56,6 +49,7 @@ class Requests extends Component {
               <Table.HeaderCell>Description</Table.HeaderCell>
               <Table.HeaderCell>Beneficary</Table.HeaderCell>
               <Table.HeaderCell>Value</Table.HeaderCell>
+              <Table.HeaderCell>Approvers</Table.HeaderCell>
             </Table.Row>
           }
           renderBodyRow={(data, index) => {
@@ -65,6 +59,9 @@ class Requests extends Component {
                 <Table.Cell>{data.desc}</Table.Cell>
                 <Table.Cell>{data.beneficary}</Table.Cell>
                 <Table.Cell>{data.value} ETH</Table.Cell>
+                <Table.Cell>
+                  {data.appoversCount}/{requestCount}
+                </Table.Cell>
                 <Table.Cell>
                   <Button color="green">Approve</Button>
                   <Button color="teal" disabled>
