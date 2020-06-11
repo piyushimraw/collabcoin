ef5080c8d395e3df09d926f715c1146137331069
piyush shrivastava <piyush@leena.ai | piyush.srivstatavaimraw@gmail.com>
2020-06-11 22:42:32 +0530

    type(Feat): fixed next build issue

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  target: 'serverless'
+  target: 'serverless',
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    // Important: return the modified config
+    config.plugins.push(new webpack.IgnorePlugin(/^(?:electron|ws)$/));
+    return config;
+  },
+  webpackDevMiddleware: (config) => {
+    // Perform customizations to webpack dev middleware config
+    // Important: return the modified config
+    return config;
+  },
 };
