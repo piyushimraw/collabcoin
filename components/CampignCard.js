c5f0fb8242afdd744e2129a16d61d7e07b9e1e74
piyush shrivastava <piyush@leena.ai | piyush.srivstatavaimraw@gmail.com>
2020-06-11 23:29:09 +0530

    type(feat): changed routing login

--- components/CampignCard.js
+++ components/CampignCard.js
@@ -3,9 +3,11 @@ import { Card, Button } from 'semantic-ui-react';
 import Link from 'next/link';
 
 export default function CampignCard({ campigns = [] }) {
-  const items = campigns.map(campign => ({
+  const items = campigns.map((campign) => ({
     header: campign,
-    description: <Link href={`/campign/${campign}`}>View Campign</Link>
+    description: (
+      <Link href={`/campign/show?address=${campign}`}>View Campign</Link>
+    ),
   }));
   return <Card.Group items={items} itemsPerRow={1} />;
 }
