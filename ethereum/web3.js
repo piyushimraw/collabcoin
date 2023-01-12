f4f884abadfd63bd83f0b578c8fbe40f19083d9d
piyush <piyush.srivastavaimraw@gmail.com>
2023-01-12 13:49:55 +0530

    Update web3.js

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,6 +1,6 @@
 import Web3 from 'web3';
 const provider = new Web3.providers.HttpProvider(
-  'https://rinkeby.infura.io/v3/4fc2cf2f0b7a4e7bbd348dbd1feb8916'
+ 'https://goerli.infura.io/v3/4fc2cf2f0b7a4e7bbd348dbd1feb8916'
 );
 let web3 = new Web3(provider, null, { transactionConfirmationBlocks: 1 });
 
